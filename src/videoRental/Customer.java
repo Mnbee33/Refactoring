d69a94b0b3f6c141e39299f1d66c4bda51e4a029
Manabe Misato <mnbee33@gmail.com>
2019-09-13 11:41:12 +0900

    リファクタリング前のstatementを追加

--- src/videoRental/Customer.java
+++ src/videoRental/Customer.java
@@ -18,4 +18,37 @@ public class Customer {
     public String getName() {
         return name;
     }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRentarPoints = 0;
+        String result = "Rental Record for" + getName() + "\n";
+
+        for (Rental each : rentals) {
+            double thisAmount = 0;
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDayRented() > 2)
+                        thisAmount += (each.getDayRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDayRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5;
+                    if (each.getDayRented() > 3)
+                        thisAmount += (each.getDayRented() - 3) * 1.5;
+                    break;
+            }
+            frequentRentarPoints++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    each.getDayRented() > 1) frequentRentarPoints++;
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRentarPoints + " frequent renter points";
+        return result;
+    }
 }
