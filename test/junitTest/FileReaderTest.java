34bda7902a1aae12e221e7e012cc8cc1d44ba9fe
Manabe Misato <mnbee33@gmail.com>
2019-09-21 11:22:26 +0900

    第4章のテスト終了

--- test/junitTest/FileReaderTest.java
+++ test/junitTest/FileReaderTest.java
@@ -4,22 +4,33 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FileReaderTest {
     private FileReader input;
+    private FileReader empty;
 
     @BeforeEach
     void setUp() {
         try {
             input = new FileReader("data.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("テストファイルがopenできない");
+            empty = newEmptyFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private FileReader newEmptyFile() throws IOException {
+        File empty = new File("empty.txt");
+        FileOutputStream out = new FileOutputStream(empty);
+        out.close();
+        return new FileReader(empty);
     }
 
     @AfterEach
@@ -46,6 +57,29 @@ public class FileReaderTest {
         for (int i = 0; i < 10; i++) {
             ch = (char) input.read();
         }
-        assertEquals(-1, input.read());
+        assertEquals(-1, input.read(), "read at end");
+    }
+
+    @Test
+    void testReadBoundaries() throws Exception {
+        assertEquals('a', input.read(), "read first char");
+        int ch;
+        for (int i = 0; i < 8; i++) {
+            ch = (char) input.read();
+        }
+        assertEquals('j', input.read(), "read last char");
+        assertEquals(-1, input.read(), "read at end");
+        assertEquals(-1, input.read(), "read past end");
+    }
+
+    @Test
+    void testEmptyRead() throws Exception {
+        assertEquals(-1, empty.read());
+    }
+
+    @Test
+    void testReadAfterClose() throws Exception {
+        input.close();
+        assertThrows(IOException.class, () -> input.read());
     }
 }
