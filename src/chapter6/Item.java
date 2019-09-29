d0cea2c51f481a61a493d16af603ae0693b1b7ee
Manabe Misato <mnbee33@gmail.com>
2019-09-29 12:30:15 +0900

    第6章 問い合わせによる一時変数の置き換え リファクタリング 前

---
+++ src/chapter6/Item.java
@@ -0,0 +1,17 @@
+package chapter6;
+
+public class Item {
+    int quantity;
+    int itemPrice;
+
+    double getPrice() {
+        int basePrice = quantity * itemPrice;
+        double discountFactor;
+        if (basePrice > 1000) {
+            discountFactor = 0.95;
+        } else {
+            discountFactor = 0.98;
+        }
+        return basePrice * discountFactor;
+    }
+}
