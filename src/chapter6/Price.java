7bebe9b4bfcd975aa2baca08d8261a2796c227e8
Manabe Misato <mnbee33@gmail.com>
2019-10-02 11:29:41 +0900

    第6章 パラメータへの代入除去 リファクタリング 前

---
+++ src/chapter6/Price.java
@@ -0,0 +1,19 @@
+package chapter6;
+
+public class Price {
+    int discount(int inputVal, int quantity, int yearToDate) {
+        if (inputVal > 50) {
+            inputVal -= 2;
+        }
+
+        if (quantity > 100) {
+            inputVal -= 1;
+        }
+
+        if (yearToDate > 10000) {
+            inputVal -= 4;
+        }
+
+        return inputVal;
+    }
+}
