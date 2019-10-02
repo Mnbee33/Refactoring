80c7827144890b4bc04b162de2fd37e1b11f1fa1
Manabe Misato <mnbee33@gmail.com>
2019-10-02 11:43:54 +0900

    第6章 メソッドオブジェクトによるメソッドの置き換え リファクタリング 後

--- src/chapter6/Account.java
+++ src/chapter6/Account.java
@@ -2,16 +2,10 @@ package chapter6;
 
 public class Account {
     int gamma(int inputVal, int quantity, int yearToDate) {
-        int importantValue1 = (inputVal * quantity) + delta();
-        int importantValue2 = (inputVal * yearToDate) + 100;
-        if (yearToDate - importantValue1 > 100) {
-            importantValue2 -= 20;
-        }
-        int importantValue3 = importantValue2 * 7;
-        return importantValue3 - 2 * importantValue1;
+        return new Gamma(this, inputVal, quantity, yearToDate).compute();
     }
 
-    private int delta() {
+    int delta() {
         return 3;
     }
 }
