fa1b01e19d1f240c52eef783cfebe815697739be
Manabe Misato <mnbee33@gmail.com>
2019-10-03 11:35:10 +0900

    第7章 フィールドの移動 リファクタリング 前

--- src/chapter7/Account.java
+++ src/chapter7/Account.java
@@ -4,11 +4,17 @@ public class Account {
     private AccountType type;
     private int daysOverdrawn;
 
+    private double interestRate;
+
     double bankCharge() {
         double result = 4.5;
         if (daysOverdrawn > 0) {
             result += type.overdraftCharge(daysOverdrawn);
         }
         return result;
+    }
+
+    double interestForAmount_Days(double amount, int days) {
+        return interestRate * amount * days / 365;
     }
 }
