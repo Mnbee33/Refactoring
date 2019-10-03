540911d251312fc60854074a299f3347d63cc67b
Manabe Misato <mnbee33@gmail.com>
2019-10-03 11:31:41 +0900

    第7章 メソッドの移動 リファクタリング 後

--- src/chapter7/Account.java
+++ src/chapter7/Account.java
@@ -4,15 +4,11 @@ public class Account {
     private AccountType type;
     private int daysOverdrawn;
 
-    double overdraftCharge() {
-        if (type.isPremium()) {
-            double result = 10;
-            if (daysOverdrawn > 7) {
-                result += (daysOverdrawn - 7) * 0.85;
-            }
-            return result;
-        } else {
-            return daysOverdrawn * 1.75;
+    double bankCharge() {
+        double result = 4.5;
+        if (daysOverdrawn > 0) {
+            result += type.overdraftCharge(daysOverdrawn);
         }
+        return result;
     }
 }
--- src/chapter7/AccountType.java
+++ src/chapter7/AccountType.java
@@ -4,4 +4,16 @@ public class AccountType {
     public boolean isPremium() {
         return false;
     }
+
+    double overdraftCharge(int daysOverdrawn) {
+        if (isPremium()) {
+            double result = 10;
+            if (daysOverdrawn > 7) {
+                result += (daysOverdrawn - 7) * 0.85;
+            }
+            return result;
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
 }
